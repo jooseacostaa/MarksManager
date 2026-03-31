d16ffd32ad622873a45546997163321497b00059
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-03-31 17:28:17 +0100

    Building Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,4 +5,30 @@ public class Main {
 
     }
 
+    public static void mostrarMenuIntro() {
+
+        System.out.println("""
+                --- GESTOR DE CALIFICACIONES ---
+                Pulse una tecla según la opción que quiera realizar
+                1. Añadir calificación
+                2. Ver calificaciones
+                3. Calcular la media
+                4. Mostrar aprobadas
+                5. Mostrar suspendidas
+                6. Eliminar nota
+                7. Salir
+                """);
+
+    }
+
+    public static void mostrarOpciones() {
+        System.out.println("1.Añadir nota - " +
+                "2.Ver notas - " +
+                "3.Calcular media - " +
+                "4.Mostrar aprobadas - " +
+                "5.Mostrar suspendidas - " +
+                "6.Eliminar nota - " +
+                "7.Salir");
+    }
+
 }
