5540b4de9be7d6af4fbfa8321d3d3b8d4fac672e
jooseacostaa <joseacostanaranjo@hotmail.com>
2026-03-31 14:11:07 +0100

    Adding methods to GestorNotas class

--- src/GestorNotas.java
+++ src/GestorNotas.java
@@ -8,4 +8,21 @@ public class GestorNotas {
         this.notas = new ArrayList<>();
     }
 
+    public void agregarNota(String asignatura, double calificacion) {
+        notas.add(new Nota(asignatura, calificacion));
+    }
+
+    public void listarNotas() {
+
+        if (notas.isEmpty()) {
+            System.out.println("La lista de notas está vacia");
+            return;
+        }
+
+        for (int i = 0; i < notas.size(); i += 1) {
+            System.out.println((i + 1) + ". " + notas.get(i));
+        }
+
+    }
+
 }
